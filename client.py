5f06096c044518fc02862e9a4944323369b92ee1
h8k8n <hakan.sengul08@gmail.com>
2016-02-23 10:19:27 +0200

    added server.py and client.py

---
+++ client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python           # This is client.py file
+import socket
+import sys
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('127.0.0.1', 10000)
+print (sys.stderr, 'connecting to %s port %s' % server_address)
+sock.connect(server_address)
+
+try:
+    
+    # Send data
+    message = 'This is the message.  It will be repeated.'
+    print (sys.stderr, 'sending "%s"' % message)
+    sock.sendall(message)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(message)
+    
+    while amount_received < amount_expected:
+        data = sock.recv(16)
+        amount_received += len(data)
+        print (sys.stderr, 'received "%s"' % data)
+
+finally:
+    print (sys.stderr, 'closing socket')
+    sock.close()
